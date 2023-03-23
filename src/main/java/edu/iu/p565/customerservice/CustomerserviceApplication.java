f2860ed5cb0036dd5b6988c0f6cdc9ff19891a48
Jarrin-Smith <jarrinksmith@gmail.com>
2023-03-23 19:47:55 -0400

    day 2 of coding

--- src/main/java/edu/iu/p565/customerservice/CustomerserviceApplication.java
+++ src/main/java/edu/iu/p565/customerservice/CustomerserviceApplication.java
@@ -3,6 +3,13 @@ package edu.iu.p565.customerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+CRUD API
+Create
+Read
+Update
+Delete
+ */
 @SpringBootApplication
 public class CustomerserviceApplication {
 
--- src/main/java/edu/iu/p565/customerservice/controller/CustomerController.java
+++ src/main/java/edu/iu/p565/customerservice/controller/CustomerController.java
@@ -2,18 +2,43 @@ package edu.iu.p565.customerservice.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.iu.p565.customerservice.repository.CustomerRepository;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
 
 import edu.iu.p565.customerservice.model.Customer;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
-    
+
+    private CustomerRepository repository;
+
+    public CustomerController(CustomerRepository repository){
+        this.repository = repository;
+    }
+
+    // get just gets information
     @GetMapping
     public List<Customer> findAll(){
-        return null;
+        return repository.findAll();
+    }
+
+    // post is used for creating
+    // Post localhost:8080/customers -> when a request is coming in this form, this method should be executed
+    @PostMapping
+    public int create(@Valid @RequestBody Customer customer){ // RequestBody means this info is in the body
+        return repository.create(customer);
+    }
+
+    // put is used for updating
+    @PutMapping("/{id}")
+    public void update(@Valid @RequestBody Customer customer, @PathVariable int id){
+        repository.update(customer, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){ // id is in the path
+        repository.delete(id);
     }
 }
